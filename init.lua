467a237b63bf474e765b9138e0cc86eac1812689
zmv7 <72821250+zmv7@users.noreply.github.com>
2022-02-13 21:07:14 +0500

    Better formspec update system

--- init.lua
+++ init.lua
@@ -1,15 +1,17 @@
 local storage = minetest.get_mod_storage()
-local fspec = "size[10,5]field[0.3,0.5;3,1;name;Name:;]button_exit[0.5,1;2,1;load;Teleport]button_exit[0.5,2;2,1;save;Save]button_exit[0.5,3;2,1;del;Delete]button_exit[0.5,4;2,1;delall;Clear list]textarea[3.5,0;7,6;;List of saved coords:;"..storage:get_string('list').."]"
-
+function genlist()
+    local fspec = "size[10,5]field[0.3,0.5;3,1;name;Name:;]button_exit[0.5,1;2,1;load;Teleport]button_exit[0.5,2;2,1;save;Save]button[0.5,3;2,1;del;Delete]button[0.5,4;2,1;delall;Clear list]textarea[3.5,0;7,6;;List of saved coords:;"..storage:get_string('list').."]"
+    return fspec
+end
 minetest.register_chatcommand("tpl", {
     description = "Open TPList",
     func = function(param)
-core.show_formspec('tplist',fspec)
+core.show_formspec('tplist',genlist())
 end})
 
 core.register_on_inventory_open(function(inventory)
-if core.localplayer:get_control().aux1 then
-    core.show_formspec('tplist',fspec)
+if core.localplayer:get_control().aux1 and core.localplayer:get_control().sneak then
+    core.show_formspec('tplist',genlist())
 end
 end)
 
@@ -20,19 +22,17 @@ core.register_on_formspec_input(function(formname, fields)
 		if fields.save then
 		    storage:set_string(fields.name,pos)
             storage:set_string('list',storage:get_string('list')..fields.name..' at '..pos..'\n')
-            --minetest.after(0.1,function()core.show_formspec('tplist',fspec)end)
             minetest.display_chat_message('Saved "'..minetest.colorize('#FF0',fields.name)..'" at '..minetest.colorize('#FF0',pos))
         elseif fields.del then
             storage:set_string('list',storage:get_string('list'):gsub(fields.name..' at %-?%d+%p%-?%d+%p%-?%d+\n',''))
             storage:set_string(fields.name,nil)
-            minetest.after(0.1,function()core.show_formspec('tplist',fspec)end)
+            core.show_formspec('tplist',genlist())
         elseif fields.delall then
             storage:set_string('list',nil)
-            minetest.after(0.1,function()core.show_formspec('tplist',fspec)end)
+            core.show_formspec('tplist',genlist())
         elseif fields.load or fields.name then
 		    storage:get_string(fields.name)
             core.run_server_chatcommand('teleport',storage:get_string(fields.name))
 		end
- fspec = "size[10,5]field[0.3,0.5;3,1;name;Name:;]button_exit[0.5,1;2,1;load;Teleport]button_exit[0.5,2;2,1;save;Save]button_exit[0.5,3;2,1;del;Delete]button_exit[0.5,4;2,1;delall;Clear list]textarea[3.5,0;7,6;;List of saved coords:;"..storage:get_string('list').."]"
 	end
 end)
