237367b5313525ee44537a06e215742ddeb839bd
zmv7 <72821250+zmv7@users.noreply.github.com>
2022-02-21 00:43:57 +0500

    List now saving in dropdown

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 local storage = minetest.get_mod_storage()
 function genlist()
-    local fspec = "size[10,5]field[0.3,0.5;3,1;name;Name:;]button_exit[0.5,1;2,1;load;Teleport]button_exit[0.5,2;2,1;save;Save]button[0.5,3;2,1;del;Delete]button[0.5,4;2,1;delall;Clear list]textarea[3.5,0;7,6;;List of saved coords:;"..storage:get_string('list').."]"
+    local fspec = "size[9,2.5]dropdown[0,0.1;5.3,1;list;"..storage:get_string('list').." ;]button_exit[5,0;2,1;load;Teleport]field[0.3,2;5,1;name;Name:;]button_exit[5,1.7;2,1;save;Save]button[7,0;2,1;del;Delete]button[7,1.7;2,1;delall;Clear list]"
     return fspec
 end
 minetest.register_chatcommand("tpl", {
@@ -17,22 +17,19 @@ end)
 
 core.register_on_formspec_input(function(formname, fields)
 	if formname == "tplist" then
-           if fields.name then fields.name = fields.name:gsub('[;%]%[%%%%-%+%?%(%)]','') end --glitch protection!
-        local pos = core.pos_to_string(vector.round(core.localplayer:get_pos())):gsub('[()]','')
+      local pos = core.pos_to_string(vector.round(core.localplayer:get_pos())):gsub('[()]','')
+        if fields.name then fields.name = fields.name:gsub('[;%]%[%%%%-%+%?%(%)]','') end --glitch protection!
 		if fields.save then
-		    storage:set_string(fields.name,pos)
-            storage:set_string('list',storage:get_string('list')..fields.name..' at '..pos..'\n')
+            storage:set_string('list',storage:get_string('list')..fields.name..' at '..core.formspec_escape(pos)..',')
             minetest.display_chat_message('Saved "'..minetest.colorize('#FF0',fields.name)..'" at '..minetest.colorize('#FF0',pos))
         elseif fields.del then
-            storage:set_string('list',storage:get_string('list'):gsub(fields.name..' at %-?%d+%p%-?%d+%p%-?%d+\n',''))
-            storage:set_string(fields.name,nil)
+            storage:set_string('list',storage:get_string('list'):gsub(core.formspec_escape(fields.list):gsub('([^%w])','%%%1')..',',''))
             core.show_formspec('tplist',genlist())
         elseif fields.delall then
             storage:set_string('list',nil)
             core.show_formspec('tplist',genlist())
-        elseif fields.load or fields.name then
-		    storage:get_string(fields.name)
-            core.run_server_chatcommand('teleport',storage:get_string(fields.name))
-		end
+        elseif fields.load then
+             core.run_server_chatcommand('teleport',fields.list:gsub('.+at ',''))
+	end
 	end
 end)
