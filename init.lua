8d363ee10893803e221755b7b2aa15348544eeac
zmv7 <72821250+zmv7@users.noreply.github.com>
2022-03-20 22:25:46 +0500

    local function

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 local storage = minetest.get_mod_storage()
-function genlist()
+local function genlist()
     local fspec = "size[9,2.5]dropdown[0,0.1;5.3,1;list;"..storage:get_string('list').." ;]button_exit[5,0;2,1;load;Teleport]field[0.3,2;5,1;name;Name:;]button_exit[5,1.7;2,1;save;Save]button[7,0;2,1;del;Delete]button[7,1.7;2,1;delall;Clear list]"
     return fspec
 end
